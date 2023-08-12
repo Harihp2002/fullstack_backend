0282c4337f88cf34b3772b0ab301fbac132f77bd
Harihp2002 <hariprasath3290@gmail.com>
2023-08-12 14:32:01 +0530

    add fullstack_backend master

---
+++ src/main/java/com/codewithhari/fullstackbackend/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.codewithhari.fullstackbackend.repository;
+
+import com.codewithhari.fullstackbackend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+}
+
+
